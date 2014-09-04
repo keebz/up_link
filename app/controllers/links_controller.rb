e5334d972ac180651620d7043b58d9d4d0cafed6
keebz <keebz@keebz.com>
2014-09-04 11:39:32 -0700

    add title structure and create index page.

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,25 @@
+class LinksController < ApplicationController
+
+def index
+	@links = Link.all
+end
+
+def new
+end
+
+def create
+end
+
+def edit
+end
+
+def update
+end
+
+def show
+end
+
+def destroy
+end
+
+end
